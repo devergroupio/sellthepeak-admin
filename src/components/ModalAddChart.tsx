8914f2eefe89f3b356070a0a94e9aa04763b1792
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-06-24 16:30:07 +0700

    fix: add chart

--- src/components/ModalAddChart.tsx
+++ src/components/ModalAddChart.tsx
@@ -6,92 +6,92 @@ import AppContext from "../components/AppProvider";
 import Redirect from "../components/Redirect";
 
 const ModalAddChart = ({ show, onHideModal, addnewChart }) => {
-  const { user } = useContext(AppContext);
-  if (!user) {
-    return <Redirect to="/login" />;
-  }
-  const [form] = Form.useForm();
-  const gqlClient = useApolloClient();
-  useEffect(
-    () =>
-      form.setFieldsValue({
-        id: "",
-        keyword: "",
-        words: "",
-        xchars: "",
-        psa_link: "",
-        psa_line: null,
-      }),
-    []
-  );
-  const onFinish = async (values) => {
-    await gqlClient
-      .mutate({
-        mutation: INSERT_DEFINED_LIST,
-        variables: {
-          item: {
-            id: `${values.id}`,
-            keyword: `${values.keyword}`,
-            exclusion: { words: values.words, xchars: values.xchars },
-            psa_line: values.psa_line,
-            psa_link: values.psa_link,
-          },
-          on_confict: {
-            constraint: "defined_list_pkey",
-            update_columns: [],
-          },
-        },
-      })
-      .then(() => {
-        form.setFieldsValue({
-          id: "",
-          keyword: "",
-          words: "",
-          xchars: "",
-          psa_link: "",
-          psa_line: null,
-        });
-        notification.success({
-          message: "Add new chart !",
-          description: "Success",
-        });
-        onHideModal();
-        addnewChart({
-          id: `${values.id}`,
-          keyword: `${values.keyword}`,
-          exclusion: { words: values.words, xchars: values.xchars },
-          psa_line: values.psa_line,
-          psa_link: values.psa_link,
-        });
-      })
-      .catch(() =>
-        notification.error({
-          message: "Add new chart !",
-          description: "Something went wrong, please try again !",
-        })
-      );
-  };
-  return (
-    <Modal
-      title="Add New Chart"
-      visible={show}
-      onCancel={onHideModal}
-      footer={null}
-    >
-      <Form form={form} layout="vertical" onFinish={onFinish}>
-        <Form.Item
-          name="keyword"
-          label="Card"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Keyword!",
-            },
-          ]}
-        >
-          <Input type="search" placeholder="" />
-        </Form.Item>
-        <Form.Item
+	const { user } = useContext(AppContext);
+	if (!user) {
+		return <Redirect to="/login" />;
+	}
+	const [form] = Form.useForm();
+	const gqlClient = useApolloClient();
+	useEffect(
+		() =>
+			form.setFieldsValue({
+				id: "",
+				keyword: "",
+				words: "?",
+				xchars: "",
+				psa_link: "",
+				psa_line: null,
+			}),
+		[]
+	);
+	const onFinish = async (values) => {
+		await gqlClient
+			.mutate({
+				mutation: INSERT_DEFINED_LIST,
+				variables: {
+					item: {
+						id: `${values.id}`,
+						keyword: `${values.keyword}`,
+						exclusion: { words: values.words, xchars: values.xchars },
+						psa_line: values.psa_line,
+						psa_link: values.psa_link,
+					},
+					on_confict: {
+						constraint: "defined_list_pkey",
+						update_columns: [],
+					},
+				},
+			})
+			.then(() => {
+				form.setFieldsValue({
+					id: "",
+					keyword: "",
+					words: "",
+					xchars: "",
+					psa_link: "",
+					psa_line: null,
+				});
+				notification.success({
+					message: "Add new chart !",
+					description: "Success",
+				});
+				onHideModal();
+				addnewChart({
+					id: `${values.keyword.toLowerCase().replace(/[^A-Z0-9]+/gi, "")}`,
+					keyword: `${values.keyword}`,
+					exclusion: { words: values.words, xchars: values.xchars },
+					psa_line: values.psa_line,
+					psa_link: values.psa_link,
+				});
+			})
+			.catch(() =>
+				notification.error({
+					message: "Add new chart !",
+					description: "Something went wrong, please try again !",
+				})
+			);
+	};
+	return (
+		<Modal
+			title="Add New Chart"
+			visible={show}
+			onCancel={onHideModal}
+			footer={null}
+		>
+			<Form form={form} layout="vertical" onFinish={onFinish}>
+				<Form.Item
+					name="keyword"
+					label="Card"
+					rules={[
+						{
+							required: true,
+							message: "Please input your Keyword!",
+						},
+					]}
+				>
+					<Input type="search" placeholder="" />
+				</Form.Item>
+				{/* <Form.Item
           name="id"
           label="Short code"
           rules={[
@@ -113,50 +113,50 @@ const ModalAddChart = ({ show, onHideModal, addnewChart }) => {
           ]}
         >
           <Input type="search" placeholder="" />
-        </Form.Item>
-        <Form.Item
-          name="psa_link"
-          label="PSA link"
-          rules={[
-            {
-              required: true,
-              message: "Please input your PSA link!",
-            },
-          ]}
-        >
-          <Input placeholder="" />
-        </Form.Item>
-        <Form.Item
-          name="psa_line"
-          label="Card Number"
-          rules={[
-            {
-              required: true,
-              message: "Please input your PSA line!",
-            },
-          ]}
-        >
-          <Input type="number" placeholder="" />
-        </Form.Item>
-        <Form.Item name="words" label="Exclusions" rules={[]}>
-          <Input type="search" placeholder="" />
-        </Form.Item>
-        <Form.Item name="xchars" label="Special characters" rules={[]}>
-          <Input type="search" placeholder="" />
-        </Form.Item>
+        </Form.Item> */}
+				<Form.Item
+					name="psa_link"
+					label="PSA link"
+					rules={[
+						{
+							required: true,
+							message: "Please input your PSA link!",
+						},
+					]}
+				>
+					<Input placeholder="" />
+				</Form.Item>
+				<Form.Item
+					name="psa_line"
+					label="PSA line"
+					rules={[
+						{
+							required: true,
+							message: "Please input your PSA line!",
+						},
+					]}
+				>
+					<Input type="text" placeholder="" />
+				</Form.Item>
+				<Form.Item name="words" label="Exclusions" rules={[]}>
+					<Input type="search" placeholder="" />
+				</Form.Item>
+				<Form.Item name="xchars" label="Special characters" rules={[]}>
+					<Input type="search" placeholder="" />
+				</Form.Item>
 
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Add
-          </Button>
-        </Form.Item>
-      </Form>
-    </Modal>
-  );
+				<Form.Item>
+					<Button
+						type="primary"
+						htmlType="submit"
+						className="login-form-button"
+					>
+						Add
+					</Button>
+				</Form.Item>
+			</Form>
+		</Modal>
+	);
 };
 
 export default ModalAddChart;
